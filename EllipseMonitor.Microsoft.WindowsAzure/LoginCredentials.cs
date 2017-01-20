51d2a4233be04242656b7571203ffbbf3ffa09f6
pascariadrian <pascariadrian@gmail.com>
2017-01-20 18:27:20 +0200

    Add files via upload

---
+++ EllipseMonitor.Microsoft.WindowsAzure/LoginCredentials.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+
+namespace EllipseMonitor.Microsoft.WindowsAzure
+{
+    public static class LoginCredentials
+    {
+
+        private static SubscriptionCloudCredentials credentials;
+
+        public static SubscriptionCloudCredentials Credentials
+        {
+            get
+            {
+                return credentials;
+            }
+            set
+            {
+                credentials = value;
+            }
+        }
+
+        static LoginCredentials()
+        { }
+
+        public static void InitCredentials(string subscriptionId, string certThumbprint)
+        {
+
+            X509Certificate2 cert = GetCertificate(certThumbprint);
+
+            Credentials = new CertificateCloudCredentials(subscriptionId, cert);
+        }
+
+
+        #region private methods
+        private static X509Certificate2 GetCertificate(string thumbprint)
+        {
+            List<StoreLocation> locations = new List<StoreLocation> {
+                StoreLocation.CurrentUser,
+                StoreLocation.LocalMachine };
+
+            foreach (var location in locations)
+            {
+                X509Store store = new X509Store("My", location);
+                try
+                {
+                    store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+                    X509Certificate2Collection certificates = store.Certificates.Find(
+                      X509FindType.FindByThumbprint, thumbprint, false);
+
+                    if (certificates.Count == 1)
+                    {
+                        return certificates[0];
+                    }
+                    else
+                    {
+                        foreach (var cer in store.Certificates)
+                        {
+                            if (cer.Thumbprint.ToLower() == thumbprint.ToLower())
+                            {
+                                return cer;
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    store.Close();
+                }
+            }
+
+            throw new ArgumentException(string.Format(
+              "A certificate with thumbprint '{0}' could not be located.",
+              thumbprint));
+        }
+
+        #endregion
+
+    }
+}
---
+++ EllipseMonitor.Microsoft.WindowsAzure/ResourceDeployments.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.WindowsAzure.Management.Compute.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Common;
+
+namespace EllipseMonitor.Microsoft.WindowsAzure
+{
+    public class ResourceDeployments
+    {
+
+        public ResourceDeployments()
+        {
+
+
+        }
+
+
+        public DeploymentGetResponse GetDeployments(SubscriptionCloudCredentials credentials, string serviceName, DeploymentSlot deploymentSlot)
+        {
+            IComputeManagementClient ComputeManagementClient = new ComputeManagementClient();
+
+            
+            var deployments = ComputeManagementClient.Deployments.GetBySlot(serviceName, deploymentSlot);
+
+            var state = deployments.RoleInstances.First().PowerState;
+
+            return deployments;
+
+        }
+    }
+}
